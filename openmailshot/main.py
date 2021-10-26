1d963fe2f3bd84778f950b29d80092751479b0b1
Calum Andrew Morrell <calum@drulum.com>
2021-10-26 23:54:18 +0100

    initial commit

---
+++ openmailshot/main.py
@@ -0,0 +1,25 @@
+import sys
+
+from PySide6.QtCore import QCoreApplication
+from PySide6.QtWidgets import QMainWindow, QApplication
+
+from resources.MainWindow import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.action_Quit.triggered.connect(self.quit)
+
+    def quit(self):
+        # TODO: add a confirmation request?
+        QCoreApplication.exit(0)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
---
+++ openmailshot/resources/MainWindow.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'MainWindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMenu, QMenuBar,
+    QSizePolicy, QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.action_Quit = QAction(MainWindow)
+        self.action_Quit.setObjectName(u"action_Quit")
+        self.actionHandbook = QAction(MainWindow)
+        self.actionHandbook.setObjectName(u"actionHandbook")
+        self.action_About = QAction(MainWindow)
+        self.action_About.setObjectName(u"action_About")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 23))
+        self.menu_File = QMenu(self.menubar)
+        self.menu_File.setObjectName(u"menu_File")
+        self.menu_Help = QMenu(self.menubar)
+        self.menu_Help.setObjectName(u"menu_Help")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.menu_Help.sizePolicy().hasHeightForWidth())
+        self.menu_Help.setSizePolicy(sizePolicy)
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menu_File.menuAction())
+        self.menubar.addAction(self.menu_Help.menuAction())
+        self.menu_File.addAction(self.action_Quit)
+        self.menu_Help.addAction(self.actionHandbook)
+        self.menu_Help.addAction(self.action_About)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Open Mail Shot", None))
+        self.action_Quit.setText(QCoreApplication.translate("MainWindow", u"&Quit", None))
+        self.actionHandbook.setText(QCoreApplication.translate("MainWindow", u"Handbook", None))
+        self.action_About.setText(QCoreApplication.translate("MainWindow", u"&About", None))
+        self.menu_File.setTitle(QCoreApplication.translate("MainWindow", u"&File", None))
+        self.menu_Help.setTitle(QCoreApplication.translate("MainWindow", u"&Help", None))
+    # retranslateUi
+
